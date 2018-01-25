a701a3c22c4a75b8959df872536d6d583c98e457
shcke <djapedja@gmail.com>
2018-01-25 20:51:06 +0100

    Osnova programa, ukljucujuci i primer ulaza (zajedno sa izlazom) Program za sada samo ucitava fajl i ispisuje sav njegov sadrzaj u izlaz.

---
+++ HTMLTemplate.hs
@@ -0,0 +1,15 @@
+import System.IO  
+import Data.List.Split
+  
+main = htmlTemplate "HTMLExample.html"
+
+
+htmlTemplate iFile = do  
+ handle <- openFile iFile ReadMode  
+ let splitFile = splitOn "." iFile
+ let oFile = splitFile!!0 ++ "Out." ++ splitFile!!1
+ handleOut <- openFile oFile WriteMode
+ contents <- hGetContents handle  
+ hPutStr handleOut contents
+ hClose handle
+ hClose handleOut
